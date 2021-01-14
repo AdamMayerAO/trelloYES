495c71bf7aa6db2114449283720338c5873cb443
AdamMayerAO <37152342+AdamMayerAO@users.noreply.github.com>
2021-01-13 19:57:59 -0500

    Add files via upload

---
+++ Card.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// make the ReactDOM available, necessary for rendering the component
+import ReactDOM from 'react-dom';
+
+// make the App component available
+import Card from './Card';
+
+// this is the test case
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<Card />, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+    .create(<Card title = {"title"} content = {"content"}/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+})
---
+++ List.test.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+
+// make the ReactDOM available, necessary for rendering the component
+import ReactDOM from 'react-dom';
+
+// make the App component available
+import List from './List';
+
+// this is the test case
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<List cards = {[]}/>, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the UI as expected', () => {
+    const tree = renderer
+    .create(<List key = {0} header = {"B"} cards = {[]}/>)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+})
